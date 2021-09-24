555d3c05fe966af79d5591e7827b57f8cd3873fb
Sanaz M <sanazmohammaddi@gmail.com>
2021-09-24 15:57:54 +0100

    Exe 5-10

--- M1-D5 -.js
+++ M1-D5 -.js
@@ -62,10 +62,10 @@ console.log(boundary(267));
  It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
 */
 
-const strivify = function (string) {
-  if (string = )
-}
-
+const strivify = function(text){
+  if(!text.startsWith('Strive'))
+       return text+'Strive';
+  }
 /* EXERCISE 6
  Write a function called "check3and7" which accepts a positive number as a parameter and checks if it is a multiple of 3 or a multiple of 7.
  HINT: Modulus Operator
